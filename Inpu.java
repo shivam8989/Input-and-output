4b5a44404e8d676564bfe1164ff6a72604a7a199
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-03-15 14:51:09 +0530

    Add files via upload
    Input output in java

---
+++ Inpu.java
@@ -0,0 +1,13 @@
+package training;
+import java.util.*;
+public class Inpu {
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Enter your age");
+        int age = sc.nextInt();
+        System.out.println("My name is "+name+" and age is "+age);
+
+    }
+}
